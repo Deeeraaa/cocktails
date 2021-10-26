3a004e324fd088473936ed3d916ed2f6e00f3fbf
Dera <stephenie7533@gmail.com>
2021-10-26 14:42:09 +0100

    first

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla error vitae praesentium cupiditate maiores id, ratione optio, deserunt, debitis eum esse autem quibusdam. Excepturi eligendi fugiat, at quam ab quo voluptatibus hic nobis iusto inventore dolorum repellendus doloremque totam. Tenetur.</p>
+    </section>
+  )
+}
+
+export default About
